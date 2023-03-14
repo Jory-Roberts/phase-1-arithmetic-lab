c7bb2c958c7b333db2e9623e5af5cfc3f1bcd9c5
Jory Roberts <robertsjory13@gmail.com>
2023-03-14 10:37:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 62;
+let num2 = 1;
+let num3 = 5;
+let num4 = 9;
+let num5 = 20;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num4 % num3;
+
+let max = Math.max(num5, num2, num3, num4);
